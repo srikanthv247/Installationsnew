2f6783675670409f96c084418964445fa848b5ce
Charan1195 <Charan.kg006@gmail.com>
2025-06-07 13:42:26 +0530

    Updated jenkins.sh (charan)

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-sudo apt update
-sudo apt install fontconfig openjdk-17-jre -y
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins
